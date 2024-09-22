f2de70389d487064a837521f77cae779555c914b
rankaifeng <18202395273@163.com>
2024-09-22 12:46:44 +0800

    c

--- src/residence-information/index.jsx
+++ src/residence-information/index.jsx
@@ -1,38 +1,39 @@
 import { Video, View } from "@tarojs/components";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import NavigationBar from "../components/navigation-bar";
 import "./index.less";
 import ImgContent from "../components/img-content";
-import { getResidenceNoticeList } from "../api/globApi";
 import { Dialog, Popup } from "@antmjs/vantui";
+import RefreshList from "../components/refresh-list";
+import { getResidenceNoticeList } from "../api/globApi";
 export default function ResidenceInformation() {
-	const [residenceNoticeList, setResidenceNoticeList] = useState([]);
 	const [videoUrl, setVideoUrl] = useState(undefined);
-	useEffect(() => {
-		getResidenceNoticeList({ list_status: 1 }).then((res) => {
-			console.log(res);
-			setResidenceNoticeList(res.data);
-		});
-	}, []);
+	const [isLoad, setIsLoad] = useState(false);
 
 	return (
 		<View className='residence-information'>
 			<NavigationBar title='居留须知' isBackIcon={true} backIconColor='#000' titleColor='#000' isWeight={600} />
 
 			<View className='img-content-container'>
-				{residenceNoticeList && residenceNoticeList.length
-					? residenceNoticeList.map((item, index) => {
-							return (
-								<ImgContent
-									onClick={() => setVideoUrl(item.video)}
-									videoUrl={item.video}
-									type='video'
-									title={item.title}
-									time={item.update_time}
-								/>
-							);
-					  })
-					: null}
+				<RefreshList
+					getList={getResidenceNoticeList}
+					isLoad={isLoad}
+					setIsLoad={setIsLoad}
+					defaultFilter={{
+						list_status: 1,
+					}}
+					renderItem={(item, index) => {
+						return (
+							<ImgContent
+								onClick={() => setVideoUrl(item.video)}
+								videoUrl={item.video}
+								type='video'
+								title={item.title}
+								time={item.update_time}
+							/>
+						);
+					}}
+				/>
 			</View>
 			<Dialog
 				id='vanDialog3'
